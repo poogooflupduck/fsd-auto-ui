f43b8acd9b5de5381e0198b1fe0db19f0388cbbd
Karl Lee <poogooflupduck@gmail.com>
2025-05-11 17:59:28 +0000

    add UI

---
+++ app/page.js
@@ -0,0 +1,98 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-wrap font-sans bg-gray-200 p-10 pb-100">
+      <div className="m-1 w-full flex-auto rounded-lg bg-white px-2 py-2 text-gray-500">
+        <div className="flex">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>
+          <span className="ml-1 ">full-self-driving</span>
+        </div>
+      </div>
+      <div className="m-1 w-60 flex-auto rounded-lg bg-white px-2 py-2 text-gray-500">
+        <div className="flex">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21 10.5h.375c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125H21M4.5 10.5H18V15H4.5v-4.5ZM3.75 18h15A2.25 2.25 0 0 0 21 15.75v-6a2.25 2.25 0 0 0-2.25-2.25h-15A2.25 2.25 0 0 0 1.5 9.75v6A2.25 2.25 0 0 0 3.75 18Z"
+            />
+          </svg>
+          <span className="ml-1">battery: 100%</span>
+        </div>
+      </div>
+      <div className="m-1 w-40 flex-auto rounded-lg bg-white px-2 py-2 text-gray-500">
+        <div className="flex">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9 6.75V15m6-6v8.25m.503 3.498 4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 0 0-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0Z"
+            />
+          </svg>
+          <span className="ml-1">range: 400 miles</span>
+        </div>
+      </div>
+      <div className="m-1 mt-2 w-full rounded-lg">
+        <img
+          src="https://images.unsplash.com/photo-1561580125-028ee3bd62eb"
+          className="rounded-lg object-cover"
+          alt="Tailwind Play"
+        />
+      </div>
+      <div className="m-1 mt-2 flex-auto">
+        <button className="rounded-lg bg-white px-4 py-2 text-gray-500 hover:cursor-pointer hover:bg-gray-800 hover:text-white">
+          <div className="flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.362 5.214A8.252 8.252 0 0 1 12 21 8.25 8.25 0 0 1 6.038 7.047 8.287 8.287 0 0 0 9 9.601a8.983 8.983 0 0 1 3.361-6.867 8.21 8.21 0 0 0 3 2.48Z"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 18a3.75 3.75 0 0 0 .495-7.468 5.99 5.99 0 0 0-1.925 3.547 5.975 5.975 0 0 1-2.133-1.001A3.75 3.75 0 0 0 12 18Z"
+              />
+            </svg>
+            <span className="ml-1">boosted mode</span>
+          </div>
+        </button>
+      </div>
+    </div>
+  );
+}
